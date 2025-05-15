23196c11779b93cd0e4b37b1c743f48e63161de4
Materka11 <arekmaterka11@gmail.com>
2025-05-15 09:31:38 +0200

    Feature: Add nav

---
+++ src/components/Nav/Nav.styled.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 80,
+    backgroundColor: '#162C1B',
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  image: {
+    width: 105,
+    height: 32,
+    resizeMode: 'contain',
+  },
+});
--- src/stacks/Main/index.tsx
+++ src/stacks/Main/index.tsx
@@ -3,21 +3,25 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {CharacterDetailsStack} from '../CharacterDetails';
 import {TabNavigationStack} from '../TabNavigation';
 import {MainStackRoutes} from './Main.routes';
+import NavComponent from '../../components/Nav/Nav.component';
 
 const Tab = createNativeStackNavigator();
 
 export const MainStack = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        header: () => <NavComponent />,
+      }}>
       <Tab.Screen
         name={MainStackRoutes.TabNavigationStack}
         component={TabNavigationStack}
-        options={{headerShown: false}}
+        options={{headerShown: true}}
       />
       <Tab.Screen
         name={MainStackRoutes.CharacterDetailsStack}
         component={CharacterDetailsStack}
-        options={{headerShown: false}}
+        options={{headerShown: true}}
       />
     </Tab.Navigator>
   );
