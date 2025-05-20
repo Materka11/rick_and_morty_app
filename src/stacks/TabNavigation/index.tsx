41a421941a375da822f62f44ae871405df114918
Materka11 <arekmaterka11@gmail.com>
2025-05-20 10:45:23 +0200

    Feature: Implement tab navigation with custom styles and icons for Character and Favorites screens

--- src/stacks/TabNavigation/index.tsx
+++ src/stacks/TabNavigation/index.tsx
@@ -2,12 +2,48 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {CharacterListScreen} from './screens/CharacterList';
 import {FavoriteCharactersScreen} from './screens/FavoriteCharacters';
+import {Pressable, View, StyleSheet} from 'react-native';
+import DmmonoTextComponent from '../../components/DmmonoText/DmmonoText.component';
+import {Ionicons} from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigationStack = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarStyle: {
+          backgroundColor: '#224229',
+          height: 70,
+        },
+        tabBarIcon: () => {
+          let iconName: keyof typeof Ionicons.glyphMap =
+            route.name === 'Characters' ? 'person' : 'star';
+
+          return <Ionicons name={iconName} size={16} color={'white'} />;
+        },
+        tabBarLabel: () => {
+          let text =
+            route.name === 'Characters' ? 'ALL CHARACTERS' : 'LIKED CHARACTERS';
+          return (
+            <DmmonoTextComponent style={{color: '#fff'}}>
+              {text}
+            </DmmonoTextComponent>
+          );
+        },
+        tabBarButton: props => {
+          const {onPress, accessibilityState, children} = props;
+          const isFocused = accessibilityState?.selected;
+
+          return (
+            <Pressable
+              onPress={onPress}
+              style={[styles.tabButton, isFocused && styles.tabButtonFocused]}>
+              {children}
+            </Pressable>
+          );
+        },
+      })}>
       <Tab.Screen
         name="Characters"
         component={CharacterListScreen}
@@ -21,3 +57,14 @@ export const TabNavigationStack = () => {
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  tabButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tabButtonFocused: {
+    backgroundColor: '#162C1B',
+  },
+});
